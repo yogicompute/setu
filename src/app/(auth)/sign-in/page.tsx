6b74bef2969d92c51e2f36d5129fed413994088d
yogender <yogender.sharma23@lpu.in>
2025-05-29 12:57:29 +0530

    auth ui partially

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const page = () => {
+	return <SignInView />;
+};
+
+export default page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const page = () => {
+	return <SignUpView />;
+};
+
+export default page;
