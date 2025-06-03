e884843de89f53a67a3802428d818244d8bcc086
yogender <yogender.sharma23@lpu.in>
2025-06-03 22:10:17 +0530

    Agent form done

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,13 +1,34 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
-import { TRPCError } from "@trpc/server";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { agentsInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-	getMany: baseProcedure.query(async () => {
+	// Todo: change getmany, getone to use protected procedure
+	getOne: protectedProcedure
+		.input(z.object({ id: z.string() }))
+		.query(async ({ input }) => {
+			const [existingAgent] = await db
+				.select()
+				.from(agents)
+				.where(eq(agents.id, input.id));
+
+			return existingAgent;
+		}),
+	getMany: protectedProcedure.query(async () => {
 		const data = await db.select().from(agents);
-		// await new Promise((resolve) => setTimeout(resolve, 5000));
-		// throw new TRPCError({ code: "BAD_REQUEST" });
+
 		return data;
 	}),
+	create: protectedProcedure
+		.input(agentsInsertSchema)
+		.mutation(async ({ input, ctx }) => {
+			const [createdAgent] = await db
+				.insert(agents)
+				.values({ ...input, userId: ctx.auth.user.id })
+				.returning();
+			return createdAgent;
+		}),
 });
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,22 +1,37 @@
-import { initTRPC } from '@trpc/server';
-import { cache } from 'react';
+import { initTRPC, TRPCError } from "@trpc/server";
+import { cache } from "react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 export const createTRPCContext = cache(async () => {
-  /**
-   * @see: https://trpc.io/docs/server/context
-   */
-  return { userId: 'user_123' };
+	/**
+	 * @see: https://trpc.io/docs/server/context
+	 */
+	return { userId: "user_123" };
 });
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
 const t = initTRPC.create({
-  /**
-   * @see https://trpc.io/docs/server/data-transformers
-   */
-  // transformer: superjson,
+	/**
+	 * @see https://trpc.io/docs/server/data-transformers
+	 */
+	// transformer: superjson,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+
+	if (!session)
+		throw new TRPCError({
+			code: "UNAUTHORIZED",
+			message: "You are not authorized to browse this route ",
+		});
+
+	return next({ ctx: { ...ctx, auth: session } });
+});
