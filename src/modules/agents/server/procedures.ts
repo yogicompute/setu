bf007e694bb50846d38d501d01a85c1d6128268e
yogender <yogender.sharma23@lpu.in>
2025-06-04 16:24:29 +0530

    agents table done

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,7 +3,7 @@ import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
 	// Todo: change getmany, getone to use protected procedure
@@ -11,7 +11,10 @@ export const agentsRouter = createTRPCRouter({
 		.input(z.object({ id: z.string() }))
 		.query(async ({ input }) => {
 			const [existingAgent] = await db
-				.select()
+				.select({
+					...getTableColumns(agents),
+					meetingCount: sql<number>`5`,
+				})
 				.from(agents)
 				.where(eq(agents.id, input.id));
 
