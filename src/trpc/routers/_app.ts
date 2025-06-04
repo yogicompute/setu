fc3633bb5263ec68babd489836bbe82b9724a159
yogender <yogender.sharma23@lpu.in>
2025-06-04 23:17:46 +0530

    meeting schema, db push and get many get one done

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
 import { agentsRouter } from "@/modules/agents/server/procedures";
 import { createTRPCRouter } from "../init";
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
 export const appRouter = createTRPCRouter({
 	agents: agentsRouter,
+	meetings: meetingsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
