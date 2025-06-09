3acdd3f1e8d5371819fba40ed36e357c2514220c
yogender <yogender.sharma23@lpu.in>
2025-06-09 23:11:32 +0530

    recording and summary section done

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -12,19 +12,9 @@ import {
 	LoaderIcon,
 } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
-import humanizeDuration from "humanize-duration";
 import { format } from "date-fns";
-import { cn } from "@/lib/utils";
+import { cn, formatDuration } from "@/lib/utils";
 
-type Meeting = MeetingGetMany[number];
-
-function formatDuration(seconds: number) {
-	return humanizeDuration(seconds * 1000, {
-		largest: 1,
-		round: true,
-		units: ["h", "m", "s"],
-	});
-}
 
 const statusIconMap = {
 	upcoming: ClockArrowUpIcon,
--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -17,6 +17,7 @@ import { UpcomingState } from "../components/upcoming-state";
 import { ActiveState } from "../components/active-state";
 import { CancelledState } from "../components/cancelled-state";
 import { ProcessingState } from "../components/processing-state";
+import { CompletedState } from "../components/completed-state";
 
 interface Props {
 	meetingId: string;
@@ -79,7 +80,7 @@ export const MeetingIdView = ({ meetingId }: Props) => {
 				/>
 				{isCancelled && <CancelledState />}
 				{isProcessing && <ProcessingState />}
-				{isCompleted && <div>Completed</div>}
+				{isCompleted && <CompletedState data={data} />}
 				{isActive && <ActiveState meetingId={meetingId} />}
 				{isUpcoming && (
 					<UpcomingState
